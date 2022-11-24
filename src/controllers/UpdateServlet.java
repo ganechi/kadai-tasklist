e25332a5b65b0e82243fb613d28dd964b46e09f6
ganechi <ganechi01@gmail.com>
2022-11-24 17:52:59 +0900

    UpdateServlet.java

---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+    }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String _token = request.getParameter("_token");
+
+    if(_token != null && _token.equals(request.getSession().getId())) {
+      EntityManager em = DBUtil.createEntityManager();
+
+      // セッションスコープからtask_id
+      Tasks t = em.find(Tasks.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+      // フォームの内容を各フィールドに上書き
+      String content = request.getParameter("content");
+      t.setContent(content);
+
+      Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+      t.setUpdated_at(currentTime);
+
+      em.getTransaction().begin();
+      em.getTransaction().commit();
+      em.close();
+
+      // 不要なtask_idをdelete
+      request.getSession().removeAttribute("task_id");
+
+      response.sendRedirect(request.getContextPath()+"/index");
+    }
+  }
+
+}
